eae5c7d323b090cd659971723a144e0471aaaba4
Mihai-Cristian Condrea <d4rk7355608@gmail.com>
2026-08-22 10:37:14 +0300

    Register dialog in docs bundle

--- src/docs-entry.ts
+++ src/docs-entry.ts
@@ -4,6 +4,7 @@ import './components/app-bar/ws-app-bar.js';
 import './components/drawer/ws-drawer.js';
 import './components/drawer/ws-drawer-item.js';
 import './components/drawer/ws-drawer-category.js';
+import './components/dialog/ws-dialog.js';
 import './components/brand/ws-brand-mark.js';
 import './components/tabs/ws-tabs.js';
 import './components/tabs/ws-tab.js';
