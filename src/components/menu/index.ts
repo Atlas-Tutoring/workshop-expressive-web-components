b6f63e906b6eb299f7ee300c794c0cced572d9b2
Mihai-Cristian Condrea <d4rk7355608@gmail.com>
2026-08-24 20:11:48 +0300

    Export Workshop action menu

---
+++ src/components/menu/index.ts
@@ -0,0 +1,9 @@
+export {WsMenu} from './ws-menu.js';
+export type {
+  WsMenuPlacement,
+  WsMenuSelectDetail,
+  WsMenuSize,
+  WsMenuVariant,
+} from './ws-menu.js';
+export {WsMenuItem} from './ws-menu-item.js';
+export type {WsMenuItemTone} from './ws-menu-item.js';
